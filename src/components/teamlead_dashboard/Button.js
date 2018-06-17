7616386d876360c3a753cbf0ab1581f2c66a7896
Owen Ekhator <oe52@gmail.com>
2018-06-17 16:21:34 -0500

    added a  reuseable button component

--- src/components/teamlead_dashboard/Button.js
+++ src/components/teamlead_dashboard/Button.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 import './Button.css'
 
  const Button = (props) => {
    
   return (
-   
-    <button className="button" style={{backgroundColor: props.bgColor }}>{props.label}</button>
-   
+    <button className="button" type={props.type||null} onClick={props.method?()=>props.method():null} style={{backgroundColor: props.bgColor }}>{props.label}</button>
   )
 }
 
