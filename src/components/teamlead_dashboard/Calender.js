2ddc737bc9038d5beb90135696c1bfb6594050f3
Owen Ekhator <oe52@gmail.com>
2018-06-20 22:50:06 -0500

    refactoring and styling

--- src/components/teamlead_dashboard/Calender.js
+++ src/components/teamlead_dashboard/Calender.js
@@ -3,7 +3,11 @@ import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import axios from 'axios';
 import Button from './Button'
+
+
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+
 
 BigCalendar.momentLocalizer(moment);
 
@@ -22,37 +26,67 @@ export default class Calendar extends Component {
     componentDidMount(){
         const CALENDAR_ID = 'qlti16i9bd6f770k7ddqbnqb04@group.calendar.google.com'
         const API_KEY = 'AIzaSyCwxvF1cnObuETDts-TPLDXBxZbNwkOjmY' 
-        axios.get(`https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`)
-             .then(response=> {
-                
-                if(response.data.items){
-                 let event =   response.data.items.map(element=>{
-                     
-                        return {
-                            title: element.summary,
-                            start: moment(element.start.date).toDate(),
-                            end: moment(element.end.date).toDate(), 
-                        }
+        axios.get(`/api/getEvents/${this.props.match.params.id}`)
+        .then(response=>{
+            let arr2 = [];
+            let arr = this.state.events.slice();
+            //arr.push(...response.data)
+            let result =  response.data.map(element=>{
+                return   {  
+                    title: element.title,
+                    end: moment(element.end).toDate(),
+                    start: moment(element.start).toDate()
+                }
+            })
+            arr2.push(...result)
+            this.setState({events: arr2})
+            
+            axios.get(`https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`)
+                 .then(response=> {
+                    if(response.data.items){
+                     let event =   response.data.items.map(element=>{
+                         
+                            return {
+                                title: element.summary,
+                                start: moment(element.start.date).toDate(),
+                                end: moment(element.end.date).toDate(), 
+                            }
+                        })
+                       
+                        let arr = this.state.events.slice()
+                        arr.push(...event)
+                        this.setState({events: arr})
+                    } 
+                })
+                    
                     })
-                   
-                    let arr = this.state.events.slice()
-                    arr.push(...event)
-                    this.setState({events: arr})
-                } 
-               })
+
 
     }
 
     addEvent= (e)=>{
-        e.preventDefault()    
+        e.preventDefault()  
+
+        //add event to calendar  
         let date ={
                 title: this.state.title,
                 start: moment(this.state.startDate).toDate(),
                 end: moment(this.state.endDate).toDate(),
         }
+
         let events = this.state.events.slice()
             events.push(date)
             this.setState({events: events})
+            
+            //save calendar to database
+                
+            let obj ={
+                projId: this.props.match.params.id,
+                ...date
+            }
+
+            axios.post('/api/addEvent',obj)
+                  .then(response=>console.log(response.data))
    }
 
    showFields=()=>{
@@ -79,14 +113,15 @@ export default class Calendar extends Component {
                              }}
         events={this.state.events}  
         />
+
         { this.state.addEvent &&
         <form onSubmit={this.addEvent}>
         <label>Title:</label>
         <input value={this.state.title} name="title" onChange={this.onChange}/>
         <label>start date </label>
-        <input value={this.state.startDate} type="date" name="startDate" onChange={this.onChange}/>
+        <input value={this.state.startDate} type="datetime-local" name="startDate" onChange={this.onChange}/>
         <label>End Date</label>
-        <input value={this.state.endDate} type="date" name="endDate" onChange={this.onChange}/>
+        <input value={this.state.endDate} type="datetime-local" name="endDate" onChange={this.onChange}/>
         <Button
         bgColor="blue"
         label="Add"
