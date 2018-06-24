a18b1828d21c3f1c8a83505c3893b4575e07fb16
Owen Ekhator <oe52@gmail.com>
2018-06-23 21:58:15 -0500

    added reducer to store

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -5,6 +5,8 @@ import SignupReducer from "./teamLead/SignupReducer";
 import ViewProfileReducer from "./teamLead/ViewProfileReducer.js";
 import AddProjectReducer from "./teamLead/AddProjectReducer";
 import GetProjectsReducer from './teamLead/GetProjectsReducer'
+import GetPeopleReducer from './teamMember/GetPeopleReducer'
+import GetTaskReducer from './task/GetTaskReducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -14,7 +16,9 @@ const combinedReducers = combineReducers({
   Signup: SignupReducer,
   ViewProfile: ViewProfileReducer,
   AddProject: AddProjectReducer,
-  GetProject: GetProjectsReducer
+  GetProject: GetProjectsReducer,
+  GetPeople: GetPeopleReducer,
+  GetTask: GetTaskReducer
 });
 
 //const middlewares = composeEnhancers(applyMiddleware(promiseMiddleware()));
